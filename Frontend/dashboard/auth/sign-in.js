07ad3e3ce015f2ddf86c5eeb7f9b73e330a04c95
yasiyaa <yasithvidusara@gmail.com>
2023-10-30 22:36:31 +0530

    login validate

--- Frontend/dashboard/auth/sign-in.js
+++ Frontend/dashboard/auth/sign-in.js
@@ -1,34 +1,43 @@
 var login = new Vue({
-    el: "#loginform",
-    data: {
-        email: '',
-        password: ''
+  el: "#loginform",
+  data: {
+    email: "",
+    password: "",
+  },
+  mounted() {},
+  updated() {},
+  methods: {
+    authenticate: function () {
+      var credentials = { email: this.email, password: this.password };
+
+      // Validation flags
+      var valid = true;
+
+      if (this.email.trim() === "") {
+        alert("Email is required");
+        valid = false;
+      } 
+
+      if (this.password.trim() === "") {
+        alert("Password is required");
+        valid = false;
+      }
+
+      if (valid) {
+      
+          axios
+            .post("http://localhost:5000/customer/authenticate", credentials)
+            .then((res) => {
+              if (res.status == 200) {
+                window.location.href = "../index.html";
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+              alert("Incorrect email or password");
+            });
+      }
+
     },
-    mounted() {
-        
-    },
-    updated() {
-        
-    },
-    methods:{
-  
-        authenticate: function(){
-  
-        var credentials  = { email: this.email, password: this.password};
-  
-        axios.post('http://localhost:5000/customer/authenticate',credentials)
-        .then((res) => {
-            
-            if(res.status == 200){
-                window.location.href = '../index.html'
-                
-            }
-  
-        })
-        .catch((err)=>{
-            console.log(err)
-            alert('Incorrect email or password');
-        });
-        }
-    }
-  })
+  },
+});
